f929a1460997df32a753d0d61b0e199486e3ca80
Jair <web@layoutz.com.br>
2014-03-24 21:50:03 -0300

    Fixed bug with the sql file missing the table prefix variable

--- struct.sql
+++ struct.sql
@@ -3,7 +3,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for oc_t_lzto_user_settings
 -- ----------------------------
 DROP TABLE IF EXISTS /*TABLE_PREFIX*/t_lzto_user_settings;
-CREATE TABLE oc_t_lzto_user_settings (
+CREATE TABLE /*TABLE_PREFIX*/t_lzto_user_settings (
   s_ip int(11) unsigned NOT NULL,
   s_name varchar(50) NOT NULL,
   s_settings text,
